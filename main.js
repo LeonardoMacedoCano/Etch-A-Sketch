2bde2fb1b46cc6ca7836817c46d4ba46f82b4112
LCano <leonardomacedocano@gmail.com>
2020-06-26 22:19:21 -0300

    add declare const and let

---
+++ main.js
@@ -0,0 +1,21 @@
+const
+  canvas = document.querySelector('canvas'),
+  context = canvas.getContext('2d'),
+  moveDistance = 10,
+  height  = canvas.height,
+  width = canvas.width;
+
+let
+  x = Math.floor(Math.random() * width),
+  y = Math.floor(Math.random() * height);
+
+with (context) {
+  lineJoin = 'round';
+  lineCap = 'round';
+  lineWidth = moveDistance;
+
+  beginPath();
+  moveTo(x, y);
+  lineTo(x, y);
+  stroke();
+}
