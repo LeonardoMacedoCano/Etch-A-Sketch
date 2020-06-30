e94cf56616b9caef08bc3bd61939886c1600229c
LCano <leonardomacedocano@gmail.com>
2020-06-29 23:35:39 -0300

    add function to menu visibility

--- main.js
+++ main.js
@@ -79,3 +79,25 @@ function keyPress(event) {
 }
 
 window.addEventListener('keydown', keyPress);
+
+//Visibilidade Menu
+$(document).ready(function(e){
+  var windowHeight = window.innerHeight;
+  $('#navigation-menu').css('height', windowHeight + 'px');
+
+  $(document).on('click', '#showMenu', function(){
+    $('#navigation-menu').css('left', '0');
+
+    $('#showMenu, #hideMenu').css('left', '160px');
+    $('#showMenu, #hideMenu').html('<i class="fa fa-chevron-left"></i>');
+    $('#showMenu').attr('id', 'hideMenu');
+  });
+
+  $(document).on('click', '#hideMenu', function(){
+    $('#navigation-menu').css('left', '-160px');
+
+    $('#showMenu, #hideMenu').css('left', '0');
+    $('#showMenu, #hideMenu').html('<i class="fa fa-cog"></i>');
+    $('#hideMenu').attr('id', 'showMenu');
+  });
+});
